ae9bf14203e5a198a6d5d30e66c099365a3c7884
Kanishka1179 <kanishka1179.becse24@chitkara.edu.in>
2025-03-09 03:20:48 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -5,21 +5,17 @@ scanf("%d", &x);
 if (x>=90) {
         printf("A");
     }
-int x;
- else if (x>=80 && X<90) {
+ else if (x>=80 && x<90) {
         printf("B");
     }
-else if (x>=70 && X<80){
+else if (x>=70 && x<80){
         printf("C");
 
-}else if (x>=60 && X<70){
+}else if (x>=60 && x<70){
         printf("D");
     }
 else if (x<60) {
     printf("F");
 }
 return 0;
-}
-
-    return 0;
 }
