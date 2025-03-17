e116a3737d7498a4c88b29de7321cbd8fb39e01f
Kanishka1179 <kanishka1179.becse24@chitkara.edu.in>
2025-03-17 19:37:32 +0530

    Solution to Calculate Average Marks of Students

---
+++ Calculate Average Marks of Students@@67877d61dc107486ad5c2743/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int data;
+    scanf("%d", &data);
+
+    int Student;
+    scanf("%d\n",&Student);
+    
+    int roll_no;
+    scanf("%d", &roll_no);
+
+    char Name;
+    scanf("%c", &Name);
+    float Marks;
+    scanf("%f", &Marks);
+
+    
+
+
+
+}
